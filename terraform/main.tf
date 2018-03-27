52d9be9ef232802d227f32e722907b0927055217
Tishkov Pavel <tishkov.pavel@gmail.com>
2018-03-27 15:46:24 +0300

    hw7: *

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,11 @@ provider "google" {
 }
 
 
+resource "google_compute_project_metadata_item" "default" {
+  key = "ssh-keys"
+  value = "appuser1:${file(var.public_key_path)}appuser2:${file(var.public_key_path)}"
+}
+
 resource "google_compute_instance" "app" {
   name         = "reddit-app"
   machine_type = "g1-small"
