3cbe5f06088ed2bd3e23d8e55b63c098ba2e5507
Tishkov Pavel <tishkov.pavel@gmail.com>
2018-03-30 16:24:53 +0300

    an attempt to guess what Travis CI needs from me v1

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -4,12 +4,14 @@ provider "google"
   project = "${var.project}"
   region  = "${var.region}"
 }
+
 module "storage-bucket"
  {
   source  = "SweetOps/storage-bucket/google"
   version = "0.1.1"
   name = ["fl64-terraform-backend", "fl64-terraform-backend2"]
 }
+
 output storage-bucket_url
  {
   value = "${module.storage-bucket.url}"
